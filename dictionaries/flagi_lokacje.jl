76a25fc83c7d5e35ef031093321cf272a8368d30
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-05-12 18:22:39 +0200

    standaryzacja nazw, usunięcie zbędnych kolumn

---
+++ dictionaries/flagi_lokacje.jl
@@ -0,0 +1,16 @@
+
+trace_dict = Dict(
+    
+    ",,W,2400"  => "false",
+    "T,,W,2400" => "true",
+    "T,,W"      => "true",
+    ",,W"       => "false"
+)
+
+location_dict = Dict(
+
+ "AUSTIN BERGSTROM INTERNATIONAL AIRPORT, TX US" => "AUSTIN",
+ "SAN ANTONIO INTERNATIONAL AIRPORT, TX US"      => "SAN ANTONIO",
+ "DAL FTW WSCMO AIRPORT, TX US"                  => "DALLAS",
+ "HOUSTON INTERCONTINENTAL AIRPORT, TX US"       => "HOUSTON"
+)
--- preprocessing.jl
+++ preprocessing.jl
@@ -3,14 +3,13 @@ using DataFrames
 using Dates
 
 include("preprocessing_helpers.jl")
+include("dictionaries/flagi_lokacje.jl")
 
 # input
-
 df_ze = loader("dane pierwotne/zapotrzebowanie energetyczne.csv")
 df_pp = loader("dane pierwotne/pomiary pogodowe.csv")
 
 # wstępny preprocessing
-
 rename!(df_ze, lowercase.(names(df_ze)))
 if "hour ending" in names(df_ze)
     rename!(df_ze, "hour ending" => "datetime")
@@ -22,18 +21,40 @@ df_ze.datetime = DateTime.(df_ze.datetime, dateformat"mm/dd/yyyy HH:MM")
 
 names(df_ze)
 
-columns = [
-            :coast, 
-            :east,
-            :fwest,
-            :north,
-            :ncent,
-            :south,
-            :scent,
-            :west,
-            :ercot]
+columns = [:coast, :east, :fwest, :north, :ncent, :south, :scent, :west, :ercot]
 
 for col in columns
     df_ze[!, col] = parse_numbers.(df_ze[!, col])
 end
-df_ze
+
+df_pp.awnd_trace = [get(trace_dict, String(x), false)      for x in df_pp.awnd_attributes]
+df_pp.prcp_trace = [get(trace_dict, String(x), false)      for x in df_pp.prcp_attributes]
+df_pp.snow_trace = [get(trace_dict, String(x), false)      for x in df_pp.snow_attributes]
+df_pp.location   = [get(location_dict, String(x), missing) for x in df_pp.name]    
+
+#usunięcie kolumn
+cols_to_delete_pp = [:awnd_attributes, :tmax_attributes, :tmin_attributes, :prcp_attributes, :snow_attributes, :name, :station, :latitude, :longitude, :elevation]
+select!(df_pp, Not(cols_to_delete_pp))
+
+# standaryzacja nazw
+rename!(df_pp, "awnd" => "wind_speed")
+rename!(df_pp, "prcp" => "precipitation")
+rename!(df_pp, "snow" => "snowfall")
+    
+rename!(df_pp, "awnd_trace" => "t_wind_speed")
+rename!(df_pp, "prcp_trace" => "t_precipitation")
+rename!(df_pp, "snow_trace" => "t_snowfall")
+
+rename!(df_pp, "tmax" => "max_temp")
+rename!(df_pp, "tmin" => "min_temp")
+
+#zmiana kolejności
+select!(df_pp, [:date, :location, :precipitation, :t_precipitation, :wind_speed, :t_wind_speed, :snowfall, :t_snowfall, :max_temp, :min_temp])
+
+
+#!SECTION - TODO 
+# HUSTON - COAST
+# DALLAS - NORTH
+# AUSTIN - SCENT
+# SAN ANTONIO - SCENT
+# ew. ercot jako dodatkowa miara, lub w ogóle zmienna opisywana
